edf2b9a374953db9cc1e177f6d52da8a48dc66f7
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2024-03-19 16:26:42 +1100

    bootstrap

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,62 @@
-import core from "@actions/core"
-import * as toml from 'smol-toml'
-import { promises as fs } from "node:fs"
+import core from "@actions/core";
+import * as toml from "smol-toml";
+import { promises as fs } from "node:fs";
+
+const bootstrap = async () => {
+  const path = core.getInput("path", {
+    required: true,
+    trimWhitespace: true,
+  });
+
+  // Read the file
+  const file = await fs.readFile(path, "utf-8").catch((error) => {
+    throw new Error(`Failed to read the file: ${error.message}`);
+  });
+
+  const key = core.getInput("key", {
+    required: true,
+    trimWhitespace: true,
+  });
+
+  const value = core.getInput("value", {
+    required: true,
+    trimWhitespace: true,
+  });
+
+  const parsedToml = toml.parse(file);
+
+  // If the key is a string, we can just set it directly
+  // split the key by '.' and iterate through the keys to set the value
+
+  const keys = key.split(".");
+  let currentObject = parsedToml as Record<string, toml.TomlPrimitive>;
+  for (let i = 0; i < keys.length - 1; i++) {
+    if (currentObject[keys[i]] === undefined) {
+      currentObject[keys[i]] = {};
+    }
+    currentObject = currentObject[keys[i]] as Record<
+      string,
+      toml.TomlPrimitive
+    >;
+  }
+  currentObject[keys[keys.length - 1]] = value;
+  core.setOutput("result", toml.stringify(parsedToml));
+
+  // Write the file
+  await fs.writeFile(path, toml.stringify(parsedToml)).catch((error) => {
+    throw new Error(`Failed to write the file: ${error.message}`);
+  });
+
+  // Set the output
+  core.setOutput("result", toml.stringify(parsedToml));
+};
 
 try {
-    const path = core.getInput("path", {
-        required: true,
-        trimWhitespace: true,
-    })
-
-    // Read the file 
-    const file = await fs
-        .readFile(path, "utf-8")
-        .catch((error) => {
-            throw new Error(`Failed to read the file: ${error.message}`)
-        })
-
-    const key = core.getInput("key", {
-        required: true,
-        trimWhitespace: true,
-    })
-
-    const value = core.getInput("value", {
-        required: true,
-        trimWhitespace: true,
-    })
-
-    const parsedToml = toml.parse(file)
-
-    // If the key is a string, we can just set it directly
-    // split the key by '.' and iterate through the keys to set the value
-    
-        const keys = key.split('.')
-        let currentObject = parsedToml as Record<string, toml.TomlPrimitive>
-        for (let i = 0; i < keys.length - 1; i++) {
-            if (currentObject[keys[i]] === undefined) {
-                currentObject[keys[i]] = {}
-            }
-            currentObject = currentObject[keys[i]] as Record<string, toml.TomlPrimitive>
-        }
-        currentObject[keys[keys.length - 1]] = value
-        core.setOutput("result", toml.stringify(parsedToml))
-    
-    // Write the file
-    await fs
-        .writeFile(path, toml.stringify(parsedToml))
-        .catch((error) => {
-            throw new Error(`Failed to write the file: ${error.message}`)
-        })
-
-    // Set the output
-    core.setOutput("result", toml.stringify(parsedToml))
-    
+  bootstrap();
 } catch (error) {
-    if (error instanceof Error) {
-        core.setFailed(error.message)
-    } else {
-        core.setFailed("Unknown error")
-    }
+  if (error instanceof Error) {
+    core.setFailed(error.message);
+  } else {
+    core.setFailed("Unknown error");
+  }
 }
